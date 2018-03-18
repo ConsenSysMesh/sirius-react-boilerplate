d43eccf713910e0f2acb5e7046757f0f81549d02
Jason Smythe <jason@smythemail.za.net>
2018-03-18 23:55:00 +0200

    Use preloaded state where possible, otherwise use initial state

--- src/configureStore.js
+++ src/configureStore.js
@@ -6,27 +6,40 @@ import socketMiddleware from './middleware/socketMiddleware'
 import throttle from 'lodash.throttle'
 import { loadState, saveState } from './localStorage'
 
-const configureStore = (history) => {
+const configureStore = (history, initialState) => {
   // Redux DevTools
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-  const persistedState = loadState();
-  const store = createStore(
-    rootReducer,
-    persistedState,
-    composeEnhancers(
-      applyMiddleware(
-        thunkMiddleware,
-        routerMiddleware(history),
-        socketMiddleware,
-      )
+  const {persistedState, isLocalStorageSet} = loadState()
+  const composedEnhancers = composeEnhancers(
+    applyMiddleware(
+      thunkMiddleware,
+      routerMiddleware(history),
+      socketMiddleware,
     )
   )
+  let store
+  // if there is a preloaded state, then use it
+  // NOTE:: this variable `isLocalStorageSet` is only ever set once on line 41 in this file (to avoid confusion)
+  if(isLocalStorageSet) {
+    store = createStore(
+      rootReducer,
+      persistedState,
+      composedEnhancers,
+    )
+  } else {
+    store = createStore(
+      rootReducer,
+      composedEnhancers,
+    )
+    // since state will imideately be stored when it is initialised
+    localStorage.setItem('isLocalStorageSet', true)
+  }
 
   // Save the state to localStorage any time it changes, using throttle to make
   // sure this doesn't happen too often, given the use of JSON.stringify()
   store.subscribe(throttle(() => {
-    saveState(store.getState());
+    saveState(store.getState())
   }), 1000)
 
   if (process.env.NODE_ENV !== "production") {
--- src/localStorage.js
+++ src/localStorage.js
@@ -1,20 +1,21 @@
 export const loadState = () => {
-    try {
-        const serializedState = localStorage.getItem('state');
-        if (serializedState === null) {
-            return undefined;
-        }
-        return JSON.parse(serializedState);
-    } catch (err) {
-        return undefined;
+  try {
+    const serializedState = localStorage.getItem('state')
+    const isLocalStorageSet = localStorage.getItem('isLocalStorageSet')
+    if (serializedState === null) {
+      return undefined
     }
+    return {persistedState: JSON.parse(serializedState), isLocalStorageSet: (isLocalStorageSet === 'true')}
+  } catch (err) {
+    return undefined
+  }
 }
 
 export const saveState = (state) => {
-    try {
-        const serializedState = JSON.stringify(state);
-        localStorage.setItem('state', serializedState);
-    } catch (err) {
-        // Ignore write errors for now
-    }
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('state', serializedState)
+  } catch (err) {
+    // Ignore write errors for now
+  }
 }
